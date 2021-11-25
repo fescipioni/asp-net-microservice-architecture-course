c49521cdf37633640e98edebcdb7c8d3f4e0dee3
Facundo Ezequiel Scipioni <fescipioni@gmail.com>
2021-11-25 00:59:36 -0300

    Ordering.Application's validation behaviour added.

---
+++ src/AspNet.Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                IValidationContext context = new ValidationContext<TRequest>(request);
+
+                ValidationResult[] validationResults = await Task.WhenAll(_validators.Select(validator => validator.ValidateAsync(context, cancellationToken)));
+
+                List<ValidationFailure> failures = validationResults.SelectMany(result => result.Errors).Where(failure => failure != null).ToList();
+
+                if (failures.Any()) throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
